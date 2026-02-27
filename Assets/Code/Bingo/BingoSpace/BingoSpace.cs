6f078adbfa882288bf14ed65beb04006c3c5d00f
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-02-27 17:43:30 +0100

    Update: BingoSpace methods&propieties

--- Assets/Code/Bingo/BingoSpace/BingoSpace.cs
+++ Assets/Code/Bingo/BingoSpace/BingoSpace.cs
@@ -1,6 +1,43 @@
+using UnityEditor.Timeline;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
+ enum MarkState
+{
+    Unmarked,
+    Markable,
+    Marked,
+}
 public class BingoSpace
 {
-    
+    private BingoTile tile;
+    private BingoSticker number;
+    private Vector2 pos;
+    private MarkState state;
+    private BingoCard card;
+
+    public BingoTile GetTile() 
+    {
+        return tile;
+    }
+
+    public BingoSticker GetNumber()
+    {
+        return number;
+    }
+
+    // TODO: TERMINAR
+    public void Mark()
+    {
+    }
+
+    public bool IsMarkable()
+    {
+        return state == MarkState.Markable;
+    }
+
+    public bool IsMarked()
+    {
+        return state == MarkState.Marked;
+    }
 }
