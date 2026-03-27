b790819b0349defcfbd22a747d970b14b0fb909d
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-27 19:14:46 +0100

    subir bola

--- Assets/Code/Bingo/BingoBall/BingoBall.cs
+++ Assets/Code/Bingo/BingoBall/BingoBall.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class BingoBall : MonoBehaviour
+public class BingoBall
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public int number;
+    public bool isActive;
+    public virtual void Roll() {}
+    public virtual bool IsActive()
+    { 
+        return isActive;
     }
+    public virtual void Line() {}
+    public virtual void Bingo() {}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
