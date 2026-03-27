f7894bb2be6d84224a6ec287644be0c1db9b17f1
unaxrementeria <unax_rementeria@euneiz.com>
2026-03-27 20:01:46 +0100

    add: Catapulta (a medias)

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileCatapulta.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BingoTileCatapulta : BingoTile, IMarkable, IFlamable
+{
+    public void Mark()
+    {
+        BingoCard bc = GetSpace().GetCard();
+        Vector2 thisTilePos = GetSpace().GetPosition();
+
+        bc?.MarkSpace(thisTilePos + 2 * Vector2.right); //Ni idea de lo que estoy haciendo, si esta mal por favor ayuda - Unax
+
+    }
+
+    public void OnFlame()
+    {
+        Mark();
+    }
+
+    public void PostFlame()
+    {
+    }
+
+    public void PreFlame()
+    {
+    }
+
+    public void Spread()
+    {
+        //hay que implementar la funcion de esparcir el fuego
+        throw new System.NotImplementedException();
+    }
+}
