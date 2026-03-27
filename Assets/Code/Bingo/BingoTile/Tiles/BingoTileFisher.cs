eb86eb58f926252a30a9ca253a2ef695770fee33
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-27 19:55:58 +0100

    hotfish: fisher pa crearlo en el menu

--- Assets/Code/Bingo/BingoTile/Tiles/BingoTileFisher.cs
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileFisher.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "BingoTileFisher", menuName = "BingoTiles/BingoTileFisher")]
 public class BingoTileFisher : BingoTile, IMarkable, IPermeable
 {
     public void Mark()
