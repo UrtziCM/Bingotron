b9bc2bec57dc6e822f62c361d5f44ac3256fb700
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-07 12:34:12 +0200

    Add: metodo spread

---
+++ Assets/Code/Bingo/BingoTile/Methods/ExtraMethods.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public static class ExtraMethods 
+{
+    public static void Spread(BingoTile bingoTile, float prob = 50.0f)
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 thisTilePos = bingoTile.GetSpace().GetPosition();
+
+        if (Random.Range(0.0f, 100.0f) > prob)
+            return;
+
+        Vector2[] directions =
+        {
+            Vector2.up,
+            Vector2.down,
+            Vector2.left,
+            Vector2.right
+        };
+
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 pos = thisTilePos + direction;
+
+            if (bc.IsMarkable(pos))
+                continue;
+            
+            if(bc.GetSpaceAt(pos).GetTile() is IFlamable tile)
+                tile.OnFlame();
+        }
+    }
+}
