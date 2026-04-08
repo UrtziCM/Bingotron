3183f8fb35a242455ebaf4363d1a34b26df87b8c
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-08 12:45:14 +0200

    Add: canal tile

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileCanal.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileCanal", menuName = "BingoTiles/BingoTileCanal")]
+public class BingoTileCanal : BingoTile, IMarkable
+{
+    public void Mark()
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+
+        Vector2 thisTilePos = GetSpace().GetPosition();
+
+        Vector2[] directions =
+        {
+            Vector2.up,
+            Vector2.down,
+            Vector2.left,
+            Vector2.right
+        };
+
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 pos = thisTilePos + direction;
+
+            if(bc.GetSpaceAt(pos).GetTile() is IPermeable tile)
+                tile.Wet();
+        }
+
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
+}
