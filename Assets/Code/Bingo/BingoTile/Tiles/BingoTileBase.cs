24d0086f50e3bf81f99f0cf54041496f43bee2c7
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-08 11:51:58 +0200

    soy subnormal

--- Assets/Code/Bingo/BingoTile/Tiles/BingoTileBase.cs
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileBase.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class BingoTileBase
+[CreateAssetMenu(fileName = "BingoTileBase", menuName = "BingoTiles/BingoTileBase")]
+public class BingoTileBase : BingoTile, IMarkable
 {
-    
+    public void Mark()
+    {
+        BingoCard bc = GetSpace().GetCard();
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+
+        if (bc.IsMarkable(thisTilePos))
+            bc.MarkSpace(thisTilePos);
+        
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
 }
