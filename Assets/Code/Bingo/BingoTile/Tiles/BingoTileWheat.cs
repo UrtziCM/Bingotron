29aea437acc049f1cae40ebde438bb11af5f2837
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-08 12:20:36 +0200

    Add: bingotileWheat

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileWheat.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileWheat", menuName = "Bingo/Tiles/BingoTileWheat")]
+public class BingoTileWheat : BingoTile, IMarkable, IFlammable, IPermeable
+{
+    public void Mark()
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
+
+    public void OnFlame()
+    {
+        Mark();
+    }
+
+    public void PostFlame()
+    {
+    }
+
+    public void PreFlame()
+    {
+    }
+
+    public void Spread()
+    {
+        ExtraMethods.Spread(this);
+    }
+
+    public void Wet()
+    {
+        value++;
+    }
+}
