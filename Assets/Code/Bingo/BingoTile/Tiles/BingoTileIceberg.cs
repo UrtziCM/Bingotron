8ab47ad47384f7160b6b66147d470978d62b013f
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-08 15:49:38 +0200

    Add: Iceberg tile

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileIceberg.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileIceberg", menuName = "BingoTiles/BingoTileIceberg ")]
+public class BingoTileIceberg : BingoTile, IMarkable
+{
+    public void Mark()
+    {
+        BingoCard bc = GetSpace().GetCard();
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
+
+    public void OnFlame()
+    {
+        Mark();
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 thisTilePos = GetSpace().GetPosition();
+
+        Vector2[] directions =
+        {
+            Vector2.up,
+            Vector2.up + Vector2.left,
+            Vector2.up + Vector2.right,
+            Vector2.down,
+            Vector2.down + Vector2.left,
+            Vector2.down + Vector2.right,
+            Vector2.left,
+            Vector2.right
+        };
+
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 pos = thisTilePos + direction;
+
+            if(bc.GetSpaceAt(pos).GetTile() is IPermeable tile)
+                tile.Wet();
+        }
+
+    }
+}
