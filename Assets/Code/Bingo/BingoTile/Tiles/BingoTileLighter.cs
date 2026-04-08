a0b9592ce1e1628d6cd9c214cea2ce8a86a13927
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-08 15:58:29 +0200

    Add: Lighter tile

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileLighter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileLighter", menuName = "BingoTiles/BingoTileLighter")]
+public class BingoTileLighter : BingoTile, IMarkable
+{
+    public void Mark()
+    {
+        BingoCard bc = GetSpace().GetCard();
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+
+        OnFlame();
+
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
+
+    public void OnFlame()
+    {
+    }
+
+    public void PostFlame(){}
+    
+    public void PreFlame(){}
+}
