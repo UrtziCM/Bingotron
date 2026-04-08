0c1acb9d629daeea4e29a8c454cfedcc5689ea64
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-08 12:18:15 +0200

    Add: golden ratio tile

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileGoldenRatio.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileGoldenRatio", menuName = "Bingo/Tiles/BingoTileGoldenRatio")]
+public class BingoTileGoldenRatio : BingoTile, IMarkable
+{
+
+    List<int> fibonacciNumbers = new List<int> { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+
+    public void Mark()
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+        foreach (int num in fibonacciNumbers)
+        {
+            if (bc.GetSpaceAt(thisTilePos).GetNumber().value == num)
+            {
+                int AddMoney = bc.GetValueFromProperty("money") + 5;
+                bc.GetPropertyByName("money").SetValue(AddMoney);
+                break;
+            }
+        }
+    }
+}
