b2eb7731886a2835d1c1b162d6f6999ed126b89f
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-08 13:53:21 +0200

    Add: lawnmower

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileLawnmower.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileLawnmower", menuName = "Bingo/Tiles/BingoTileLawnmower")]
+public class BingoTileLawnmower : BingoTile, IMarkable, IChargeable
+{
+    [SerializeField] private Vector2 direction;
+
+    public void Mark()
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 thisTilePos = GetSpace().GetPosition();
+        ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
+
+        Discharge(bc.GetValueFromProperty("charge"));
+
+        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+    }
+    public void Discharge(int charge)
+    {
+        BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
+        Vector2 nextTilePos = GetSpace().GetPosition() + direction;
+
+        while (bc.GetSpaceAt(nextTilePos) != null)
+        {
+            if(charge < 0)
+                break;
+
+            if(bc.IsMarkable(nextTilePos))
+                if (bc.GetSpaceAt(nextTilePos).GetTile() is IMarkable tile)
+                    tile.Mark();
+
+            charge--;
+            nextTilePos += direction;
+        }
+    }
+}
