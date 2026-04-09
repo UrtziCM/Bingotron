8c109a35771c01b87eaf2aa99d7061b5c0a1da21
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-09 11:44:57 +0200

    Fix: jackpot is igamble now

--- Assets/Code/Bingo/BingoTile/Tiles/BingoTileJackpot.cs
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileJackpot.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "BingoTileJackpot", menuName = "Bingo/Tiles/BingoTileJackpot")]
-public class BingoTileJackpot : BingoTile, IMarkable
+public class BingoTileJackpot : BingoTile, IMarkable, IGamble
 {
+    public float BaseProbability => 0.5f;
+
     public void Mark()
     {
         BingoCard bc = GetSpace().GetCard();
         Vector2 thisTilePos = GetSpace().GetPosition();
         ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
 
-        if (Random.Range(0, 100) > 50)
-            sm.AddScore(sm.Score);
-        else
-            sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+        sm.AddScore(Gamble() ? sm.Score : value + bc.GetSpaceAt(thisTilePos).GetNumber().value);
+
+        bc.GetProperty(BingoCard.GAMBLER_ADDEDPROBABILITY_PROPERTY).SetValue(bc.GetPropertyValue(BingoCard.GAMBLER_ADDEDPROBABILITY_PROPERTY) + 0.01f);
+    }
+    public bool Gamble()
+    {
+        BingoCard bc = GetSpace().GetCard();
+
+        return Random.Range(0, 1) > BaseProbability + bc.GetPropertyValue(BingoCard.GAMBLER_ADDEDPROBABILITY_PROPERTY);
     }
 }
