5fc07618c4ce88fa09c9cf19e4c7cd24d595f5ba
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-12 22:21:48 +0200

    Fix: musiicbox

--- Assets/Code/Bingo/BingoTile/Tiles/BingoTileMusicBox.cs
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileMusicBox.cs
@@ -3,18 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BingoTileMusicBox", menuName = "Bingo/Tiles/MusicBox")]
 public class BingoTileMusicBox : BingoTile, IMarkable, IFlammable, IMusicable
 {
-    private bool burnt = false;
-
     public void Mark()
     {
         BingoCard bc = ServiceLocator.GetService<BingoCard>() as BingoCard;
-        Vector2 thisTilePos = GetSpace().GetPosition();
         ScoreManager sm = ServiceLocator.GetService<ScoreManager>() as ScoreManager;
 
-        if (burnt)
-            PlayNote();
-
-        sm.AddScore(value + bc.GetSpaceAt(thisTilePos).GetNumber().value + (int)bc.GetPropertyValue(BingoCard.MUSIC_ADDEDVALUE_PROPERTY));
+        sm.AddScore(value + bc.GetSpaceAt(pos).GetNumber().value + (int)bc.GetPropertyValue(BingoCard.MUSIC_ADDEDVALUE_PROPERTY));
     }
     public void PlayNote()
     {
@@ -23,9 +17,8 @@ public class BingoTileMusicBox : BingoTile, IMarkable, IFlammable, IMusicable
 
     public void OnFlame()
     {
-        burnt = true;
-        Mark();
         PlayNote();
+        Mark();
     }
 
     public void PostFlame()
