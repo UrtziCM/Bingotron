f86b0a19420eaf97a984f4b95beab73ce85996cf
Urtzi C. <urcrmedev@gmail.com>
2026-04-17 17:26:36 +0200

    update: bingoSpaces now have random ints on startup

--- Assets/BingoSpaceHandler.cs
+++ Assets/BingoSpaceHandler.cs
@@ -1,19 +1,24 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class BingoSpaceHandler : MonoBehaviour
 {
     private BingoSpace bingoSpace;
     public Vector2 positionInGrid;
     private BingoCard card;
-    public BingoSticker sticker => bingoSpace.GetNumber();
+    public BingoStickerNumeric sticker => bingoSpace.GetSticker();
     public BingoTile tile => bingoSpace.GetTile();
+
+    [SerializeField]
+    private TMP_Text stickerNumberText;
 
     void Start()
     {
-        bingoSpace = new(positionInGrid);
+        bingoSpace = new(positionInGrid, UnityEngine.Random.Range(1,91));
         card = transform.GetComponentInParent<BingoCard>();
         card.AddBingoSpace(bingoSpace);
+        stickerNumberText.text = bingoSpace.GetSticker().Number.ToString();
     }
 
     // Update is called once per frame
