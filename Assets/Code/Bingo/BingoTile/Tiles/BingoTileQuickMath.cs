fb8a3c298d9466f1c47de86eeb6b789285f5bc19
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-17 17:36:14 +0200

    Add: BingotileQuickMath

---
+++ Assets/Code/Bingo/BingoTile/Tiles/BingoTileQuickMath.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoTileQuickMath", menuName = "Bingo/Tiles/QuickMath")]
+public class BingoTileQuickMath : BingoTile, IMarkable
+{
+    [SerializeField]
+    private int addedScore = 2;
+    [SerializeField]
+    private int addedMoney = 3;
+    public void Mark()
+    {
+        if(!(GetSpace().GetSticker() is BingoStickerNumeric)) return;
+        int num = (GetSpace().GetSticker() as BingoStickerNumeric).Number;
+
+        BingoCard bc = Utils.BingoCard;
+        ScoreManager sm = Utils.ScoreManager;
+
+        if (num % 2 == 0)
+            sm.AddScore(addedScore);
+
+        if (num % 3 == 0)
+            bc.SetPropertyValue(BingoCard.MONEY_PROPERTY , bc.GetPropertyValue(BingoCard.MONEY_PROPERTY) + addedMoney);
+
+        if (num % 5 == 0)
+        {
+            foreach (BingoSpace space in Utils.GetTouchingSpacesFrom(pos))
+                if (space.GetTile() is IPermeable tile)
+                    tile.Wet();
+        }
+
+        if (num % 7 == 0)
+        { 
+            foreach (BingoSpace space in Utils.GetTouchingSpacesFrom(pos))
+                if(space.IsMarkable() && space.GetTile() is IFlammable tile)
+                    tile.OnFlame();
+        }
+
+        sm.AddScore(value + GetSpace().GetSticker().value);
+    }
+}
