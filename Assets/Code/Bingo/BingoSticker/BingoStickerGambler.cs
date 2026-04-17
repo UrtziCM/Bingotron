87f3407d8816468af3adbd0a23ff6c911b30ef03
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-04-17 18:49:12 +0200

    Add: bingostickerGambler

---
+++ Assets/Code/Bingo/BingoSticker/BingoStickerGambler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BingoStickerGambler", menuName = "Bingo/Stickers/Gambler")]
+public class BingoStickerGambler : BingoStickerNumeric, IRoller, IGamble
+{
+    public BingoStickerGambler(int number) : base(number)
+    {
+    }
+
+    public float BaseProbability => 0.01f;
+
+    public bool Gamble()
+    {
+        return Utils.Gamble(BaseProbability);
+    }
+    public void OnRoll(BingoBall ball)
+    {
+        if (GetSpace().IsMarkable())
+            if (Gamble())
+                GetSpace().Mark();
+    }
+}
