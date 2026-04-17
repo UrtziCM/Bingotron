4a9cf45057ec99654353a6bf1071d8b2a4f2036c
unaxrementeria <unax_rementeria@euneiz.com>
2026-04-17 18:02:48 +0200

    Add: Question Mark Sticker (mas o menos)

---
+++ Assets/Code/Bingo/BingoSticker/BingoStickerQuestionMark.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BingoStickerQuestionMark : BingoSticker, IRoller
+{
+    [SerializeField]
+    protected int number;
+    public int Number { get { return number; } }
+
+    public override bool IsMarkable(BingoBall ball)
+    {
+        return ball.number == number;
+    }
+
+    //faltaria implementar la parte visual de cuando cambia de valor 
+    public void OnRoll(BingoBall ball)
+    {
+        number = Random.Range(1,90);
+        throw new System.NotImplementedException();
+    }
+}
