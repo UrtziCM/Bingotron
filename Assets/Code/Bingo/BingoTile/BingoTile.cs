342286df8f93e537d2dcb39236b835080bb7f7e2
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-05-08 12:12:41 +0200

    Update: ahora las tiles tienen tags

--- Assets/Code/Bingo/BingoTile/BingoTile.cs
+++ Assets/Code/Bingo/BingoTile/BingoTile.cs
@@ -1,5 +1,15 @@
 using UnityEngine;
 
+public enum Tags
+{
+    Inflamable,
+    Cargable,
+    Musical,
+    Permeable,
+    Ludópata,
+    Conjurable
+}
+
 [CreateAssetMenu(fileName = "BingoTile", menuName = "Bingo/Tiles/BingoTile", order = -1)]
 public class BingoTile : ScriptableObject
 {
@@ -9,6 +19,8 @@ public class BingoTile : ScriptableObject
     internal int value = 1;
     [SerializeField, TextArea]
     private string Description;
+    [SerializeField]
+    private Tags[] tags;
 
     public BingoSpace GetSpace()
     {
