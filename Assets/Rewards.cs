fcaa7c7876c7ccd706bde02d0fd12858f810741e
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-06-02 19:16:50 +0200

    Update: tags en la tienda

--- Assets/Rewards.cs
+++ Assets/Rewards.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -61,9 +62,39 @@ public class Rewards : CustomService
             {
                 generatedTile = Instantiate(generatedTile);
                 generatedItem = generatedTile;
-                rewardPanel.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = generatedTile.Name;
-                rewardPanel.transform.GetChild(0).GetChild(1).GetComponent<Image>().sprite = generatedTile.sprite;
-                rewardPanel.transform.GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text = "";
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = generatedTile.Name;
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<Image>().sprite = generatedTile.sprite;
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text = "";
+
+                if (generatedTile.tags != null && generatedTile.tags.Length > 0)
+                {
+                    rewardPanel.transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+
+                    TextMeshProUGUI tagText1 = rewardPanel.transform.GetChild(0).GetChild(1).GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
+                    Image tagImage1 = rewardPanel.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>();
+                    TextMeshProUGUI tagText2 = rewardPanel.transform.GetChild(0).GetChild(1).GetChild(1).GetComponentInChildren<TextMeshProUGUI>();
+                    Image tagImage2 = rewardPanel.transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<Image>();
+
+                    tagText1.text = BingoTile.TAGS[(int)generatedTile.tags[0]];
+                    tagImage1.color = Utils.GetTagColor(generatedTile.tags[0]).WithAlpha(150);
+
+                    if (generatedTile.tags.Length > 1)
+                    {
+                        tagText2.text = BingoTile.TAGS[(int)generatedTile.tags[1]];
+                        tagImage2.color = Utils.GetTagColor(generatedTile.tags[1]).WithAlpha(150);
+                    }
+                    else
+                    {
+                        tagText2.text = "";
+                        tagImage2.color = tagImage2.color.WithAlpha(0);
+                    }
+                }
+                else
+                {
+                    //Quitar las tags
+                    rewardPanel.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+                }
+
                 rewardPanel.transform.GetChild(1).GetComponent<TMP_Text>().text = generatedTile.Description;
             } else if (generatedItem is BingoStickerNumeric generatedSticker)
             {
@@ -71,13 +102,16 @@ public class Rewards : CustomService
                 generatedItem = generatedSticker;
 
                 generatedSticker.Number = Random.Range(1, 51);
-                rewardPanel.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = generatedSticker.Name;
-                rewardPanel.transform.GetChild(0).GetChild(1).GetComponent<Image>().sprite = null;
-                rewardPanel.transform.GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text =
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = generatedSticker.Name;
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<Image>().sprite = null;
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text =
                     (generatedSticker is BingoStickerQuestionMark) ? "?" : generatedSticker.Number.ToString();
-                rewardPanel.transform.GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().color =
+                rewardPanel.transform.GetChild(0).GetChild(0).GetChild(1).GetComponentInChildren<TextMeshProUGUI>().color =
                     generatedSticker.textColor; 
                 rewardPanel.transform.GetChild(1).GetComponent<TMP_Text>().text = generatedSticker.Description;
+
+                //Quitar las tags
+                rewardPanel.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
             }
             Button thisButton = rewardPanel.transform.GetComponentInChildren<Button>(true);
             thisButton.interactable = true;
