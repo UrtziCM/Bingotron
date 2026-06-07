2d27e3e75c90c8c6cb033f55115122940b81b1bc
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-06-07 20:26:48 +0200

    ultimos arreglos ahora si que si

--- Assets/BingoDrumHelper.cs
+++ Assets/BingoDrumHelper.cs
@@ -50,7 +50,7 @@ public class BingoDrumHelper : MonoBehaviour
         
         yield return new WaitForSeconds(1f);
         //Sonido de roll
-        //Utils.AudioManager.PlayDrumRoll();
+        Utils.AudioManager.PlayDrumRoll();
 
         droppedBall = false;
         yield return new WaitUntil(() => droppedBall);
@@ -143,8 +143,8 @@ public class BingoDrumHelper : MonoBehaviour
     private IEnumerator MoveBallTowards(Transform ball, int ballNum)
     {
         //Sonido de roll
-        //Utils.AudioManager.StopDrumRoll();
-        //Utils.AudioManager.PlaySFX(Utils.AudioManager.ballSound);
+        Utils.AudioManager.StopDrumRoll();
+        Utils.AudioManager.PlaySFX(Utils.AudioManager.ballSound);
 
         while (Vector3.Distance(ball.position, ballTargetPos.position) > 0.01f)
         {
